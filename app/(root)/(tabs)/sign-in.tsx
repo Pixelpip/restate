6c9d6c00faa406964d190c3561289fe31135e0a8
Pixelpip <athulkamathg5@gmail.com>
2024-12-21 19:23:30 +0530

    msg

---
+++ app/(root)/(tabs)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const sign = () => {
+  return (
+    <View>
+      <Text> sign </Text>
+    </View>
+  )
+}
+
+export default sign
